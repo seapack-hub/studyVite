0e62cc7c0432f8855ff8c2c7fe57d1b39fba6934
SEAPACK\32709 <zenghf@tsingyun.net>
2024-02-06 17:36:14 +0800

    css模块化

--- src/views/components/componentsA.js
+++ src/views/components/componentsA.js
@@ -1,10 +1,15 @@
 //引入样式
-import './componentA.css'
+// import './componentA.css'
+import componentModule from './componentA.module.css'
+
+console.log('css模块化：',componentModule)
 //1.创建一个div元素
 let div = document.createElement('div');
 
 //2.给div元素添加一个类名并设置文本
-div.classList.add('footer');
+// div.classList.add('footer');
+div.classList.add(componentModule.footer);
+div.classList.add(componentModule.footerName);
 div.innerText = 'componentA'
 
 //3.将div添加到body上
--- vite.base.config.js
+++ vite.base.config.js
@@ -3,7 +3,15 @@ export default defineConfig({
     optimizeDeps: {
         include: [],
     },
-    css:{},
+    css:{
+        modules:{
+            localsConvention:'camelCase',
+            scopeBehaviour:'local',
+            globalModulePaths:[],
+            hashPrefix:'hello',
+            generateScopedName:"[name]_[local]_[hash:5]"
+        }
+    },
     plugins:[],
     envPrefix:'ENV',  // 配置vite注入客户端环境变量校验的env前缀
 })
